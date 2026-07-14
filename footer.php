545e05c017854395c0598c8f3e22a7ac9bf971ca
keithkirui91 <keith.kirui33@gmail.com>
2026-07-14 08:58:14 +0300

    ok
    SHIFT

--- footer.php
+++
@@ -1,8 +0,0 @@
-        </section>
-    </main>
-</div>
-<div id="toast" class="toast" hidden></div>
-<script>window.FLEETDESK_BASE = <?= json_encode(BASE_URL) ?>;</script>
-<script src="<?= e(BASE_URL) ?>/app.js?v=<?= e((string)@filemtime(__DIR__ . '/app.js')) ?>"></script>
-</body>
-</html>
--- vehicle.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/helpers.php';
-require_once __DIR__ . '/config.php';
-redirect_to('fleet.php');
